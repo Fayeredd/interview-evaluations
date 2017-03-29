ff159cb190aa166936ac52161badd368836d4ea5
Madison <madison.f.redd@live.com>
2017-03-29 12:21:49 -0700

    bugfix

--- src/main/java/com/revature/repositories/UserRepository.java
+++ src/main/java/com/revature/repositories/UserRepository.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.revature.security.repository;
+package com.revature.repositories;
 
 import com.revature.model.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Repository;
  *
  * @author FayeRedd
  */
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
